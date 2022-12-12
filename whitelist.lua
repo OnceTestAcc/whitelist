ac3fe58954f69816c1e4d7e2a4c831543e908c77
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-12-12 18:01:04 -0500

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,7 @@
+local arr = {}
+local userid = game:GetService('Players').LocalPlayer.UserId
+	if userid ~= (arr[#arr]) then
+		game.Players.LocalPlayer:Kick("Your not whitelisted")
+		wait(1)
+		game.Players.LocalPlayer.Character:BreakJoints()
+	end
